a4475ccd33c56dba69b21e34d40d317851dc01b4
CodeManMalck <51975207+CodeManMalck@users.noreply.github.com>
2025-12-03 18:39:17 -0800

    Add files via upload

---
+++ instructions.md
@@ -0,0 +1,3 @@
+Fix the error in the for loop.
+
+**Note:** This loop may run, but there's still something wrong with it.
---
+++ main.js
@@ -0,0 +1,13 @@
+/*     IGNORE THIS LINE       */      function loop() {
+
+
+
+for (x = 10; x > 0; x--) {
+    console.log(x);
+}
+
+
+
+
+
+/*      IGNORE THIS LINE          */     }; loop();
